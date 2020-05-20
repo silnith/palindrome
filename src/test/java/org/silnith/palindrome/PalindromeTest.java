8cc2385bff313cf7b9c53f9f90596274010779a0
silnith <silnith@gmail.com>
2020-05-19 17:35:34 -0700

    Test some Japanese.

--- src/test/java/org/silnith/palindrome/PalindromeTest.java
+++ src/test/java/org/silnith/palindrome/PalindromeTest.java
@@ -14,4 +14,19 @@ public class PalindromeTest {
         assertTrue(new Palindrome().isPalindrome("\uD83D\uDE01\uD83D\uDE01\uD83D\uDE01", Locale.US));
     }
     
+    @Test
+    public void testIsPalindromeJapaneseMultiplicationTable() {
+        assertTrue(new Palindrome().isPalindrome("九九", Locale.JAPAN));
+    }
+    
+    @Test
+    public void testIsPalindromeJapaneseNewspaper() {
+        assertTrue(new Palindrome().isPalindrome("新聞紙", Locale.JAPAN));
+    }
+    
+    @Test
+    public void testIsPalindromeJapaneseTomato() {
+        assertTrue(new Palindrome().isPalindrome("トマト", Locale.JAPAN));
+    }
+    
 }
